7ef55b8a44496f0e8f02ca1c143b8478c009e716
sara66zf <32036817+sara66zf@users.noreply.github.com>
2017-11-19 23:08:24 +0330

    ui register admin&list user

--- web/js/main.js
+++ web/js/main.js
@@ -8,3 +8,49 @@ function confirm()
         return false;
     }
 }
+//function register admin
+function openreg(evt, oprationName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(oprationName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+
+//page register
+function Myclick() {
+ // if(document.getElementById("name").value===""|| document.getElementById("lname").value===""||
+ //    document.getElementById("uname").value===""
+ //     || document.getElementById("pass").value===""||document.getElementById("cpass").value===""||
+ //     document.getElementById("ans1").value===""||
+ //    document.getElementById("ans2")==="" ){
+ //    ddocument.getElementById("name").style.borderColor="red";
+ //     document.getElementById("lname").style.borderColor="red";
+ //    document.getElementById("uname").style.borderColor="red";
+ //
+ //    document.getElementById("pass").style.borderColor="red";
+ //    document.getElementById("cpass").style.borderColor="red";
+ //     document.getElementById(ans1).style.borderColor="red";
+ //        document.getElementById("ans2").style.borderColor="red";
+ //     alert("please fill text boxes");
+ //
+ //
+ // }else
+    alert("register successful");
+
+}
+function myFunction() {
+    var x = document.createElement("INPUT");
+    x.setAttribute("type", "file");
+    document.body.appendChild(x);
+}
--- web/user/list.jsp
+++ web/user/list.jsp
@@ -7,10 +7,51 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
+
 <head>
-    <title></title>
+    <title>gallery</title>
+    <link href="/css/style2.css" rel="stylesheet" />
 </head>
+
 <body>
+<!-- MAIN (Center website) -->
+<div class="main">
+
+    <h1>Gallery</h1>
+
+    <!-- Portfolio Gallery Grid -->
+    <div class="row">
+        <div class="column Autumn">
+            <div class="content">
+                <a href="sublist.jsp">
+                    <img src="../images/Autumn/Autumn13.jpg"  id="1"  alt="Autumn" style="width:100%">
+                    <h4>Autumn</h4>
+                </a>
+
+            </div>
+        </div>
+
+
+        <div class="column sport">
+            <div class="content">
+                <a href="sublist.jsp">
+                    <img src="../images/sport/Sport24.jpg"   id="2" alt="sport" style="width:100%">
+                    <h4>sport</h4>
+                </a>
+            </div>
+        </div>
+
+        <div class="column spring">
+            <div class="content">
+                <a href="sublist.jsp">
+                    <img src="../images/spring/Spring113.jpg"  id="3" alt="Spring" style="width:100%">
+                    <h4>spring</h4>
+                </a>
+            </div>
+        </div>
+
+
+
 
 </body>
 </html>
